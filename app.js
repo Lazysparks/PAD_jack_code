d8b15f59d9b3969c751879814928de2cb238140a
Thomas <thomasballais@gmail.com>
2016-05-21 21:54:41 +0200

    nieuwe lijn code in app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+var wijziging = "dit is een nieuwe lijn code"
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars')
+var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 
@@ -18,8 +19,6 @@ app.engine('hbs', exphbs({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
